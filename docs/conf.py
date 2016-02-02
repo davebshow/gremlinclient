55fef6e53a0f9403759ef9f5145b9b354aeb605c
davebshow <davebshow@gmail.com>
2016-02-01 19:40:44 -0500

    fix in docs

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,7 @@ html_theme = 'alabaster'
 # documentation.
 html_theme_options = {
     # 'logo': 'logo.png',
-    'description': 'Async python client for the TP3 Gremlin Server',
+    'description': 'Async Python client for the TP3 Gremlin Server',
     'logo_text_align': 'left',
     'github_user': 'davebshow',
     'github_repo': 'gremlinclient',
